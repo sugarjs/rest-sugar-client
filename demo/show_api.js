bdcacd81c4f27f1c773c0d76d385a99ba495f744
Juho Vepsalainen <bebraw@gmail.com>
2012-06-05 13:55:18 +0300

    Remove redundant demo

--- demo/show_api.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var client = require('../lib/rest-sugar-client');
-var url = 'http://localhost:8000/api/v1';
-var APIKEY = 'dummy'; // stash this into env or so
-
-client.api(url + '?apikey=' + APIKEY, log, log);
-
-function log(d) {console.log(d);}
-

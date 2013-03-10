b5290766fcce0c26fa037d1b6f1e60a3e88ed8cc
Juho Vepsalainen <bebraw@gmail.com>
2013-03-10 16:29:59 +0200

    Make sure GET query methods are capitalized

--- tests.js
+++ tests.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+var assert = require('assert');
+
 var express = require('express');
 
 var client = require('./lib/rest-sugar-client');
@@ -17,15 +19,25 @@ function main() {
         });
     });
 
+    app.get(apiPrefix + '/libraries', function(req, res) {
+        assert.equal(req.query.method, 'GET');
+
+        res.json([{
+            name: 'demo library'
+        }]);
+    });
+
     app.listen(3000, function(err) {
         if(err) return console.error(err);
 
         client.api('http://localhost:' + port + apiPrefix, function(err, api) {
             if(err) return console.error(err);
 
-            console.log(api);
+            api.libraries.get(function(err, d) {
+                if(err) return console.error(err);
 
-            process.exit();
+                process.exit();
+            });
         });
     });
 }
